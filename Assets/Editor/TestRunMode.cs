52ee364733a843c936e9d4b3c4d7af8886dbfa18
xclouder <xclouder@qq.com>
2018-11-21 22:56:23 +0800

    add MainThreadDispatcher

---
+++ Assets/Editor/TestRunMode.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class TestRunMode {
+
+
+	// A UnityTest behaves like a coroutine in PlayMode
+	// and allows you to yield null to skip a frame in EditMode
+	[UnityTest]
+	public IEnumerator TestRunModeWithEnumeratorPasses() {
+		// Use the Assert class to test conditions.
+		// yield to skip a frame
+
+		var m = MainThreadDispatcher.Instance;
+		
+		yield return null;
+	}
+}
---
+++ Assets/LightRx/UnityBridge/Scheduler/MainThreadDispatcher.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class MainThreadDispatcher
+{
+	private static MainThreadDispatcher _ins;
+	public static MainThreadDispatcher Instance
+	{
+		get
+		{
+			Initialize();
+			return _ins;
+		}
+	}
+
+	private static void Initialize()
+	{
+		if (_ins == null)
+		{
+			_ins = new MainThreadDispatcher();
+		}
+	}
+
+	private InnerHelpMonoBehaviour _monoBehaviour;
+	private MainThreadDispatcher()
+	{
+		var obj = new GameObject("[LightRx-MainThreadDispatcher]");
+		obj.hideFlags = HideFlags.HideInHierarchy;
+		GameObject.DontDestroyOnLoad(obj);
+		
+		_monoBehaviour = obj.AddComponent<InnerHelpMonoBehaviour>();
+	}
+
+	public void StartCoroutine(IEnumerator coroutine)
+	{
+		if (InnerHelpMonoBehaviour.mainThreadToken == null)
+		{
+			throw new InvalidOperationException("can only call this method in main thread");
+		}
+
+		_monoBehaviour.StartCoroutine(coroutine);
+	}
+	
+	private class InnerHelpMonoBehaviour : MonoBehaviour
+	{
+		[ThreadStatic]
+		public static object mainThreadToken;
+
+		private void Awake()
+		{
+			mainThreadToken = new object();
+		}
+	}
+	
+}
