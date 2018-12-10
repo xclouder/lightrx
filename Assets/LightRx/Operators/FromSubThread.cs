263cff2421ac78ba67d48d08b1f3716fcebd64bc
xclouder <xclouder@qq.com>
2018-12-10 10:06:31 +0800

    add FromSubThread

---
+++ Assets/LightRx/Operators/FromSubThread.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class FromSubThread<T, TR> : IObservable<TR>
+{
+	private IObservable<T> _source;
+	
+	public FromSubThread(IObservable<T> source)
+	{
+		_source = source;
+	}
+	
+	public IDisposable Subscribe(IObserver<TR> observer)
+	{
+		return null;
+	}
+
+	private class FromSubThreadInternal : OperatorObserverBase<T, TR>
+	{
+		public FromSubThreadInternal(IObserver<TR> observer, IDisposable cancel) : base(observer, cancel)
+		{
+		}
+
+		public override void OnNext(T value)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override void OnComplete()
+		{
+			throw new NotImplementedException();
+		}
+
+		public override void OnError(Exception error)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
