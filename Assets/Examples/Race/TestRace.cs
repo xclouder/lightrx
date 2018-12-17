60927822dddb0e27feb2861e5ed69ffbfa4b9e6b
xclouder <xclouder@qq.com>
2018-12-17 13:00:46 +0800

    add TestRace

--- Assets/Examples/Race/TestRace.cs
+++ Assets/Examples/Race/TestRace.cs
@@ -1,12 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using LightRx;
 using UnityEngine;
 
 public class TestRace : MonoBehaviour {
 
 	void Start()
 	{
-		//TODO
+		Observable.Race(
+				ObservableWWW.Get("http://www.zhihu.com"),
+				ObservableWWW.Get("http://www.qq.com")
+			)
+			.Subscribe(v =>
+			{
+				Debug.Log("get content:" + v);
+			});
 	}
 	
 }
